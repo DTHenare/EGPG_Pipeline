a02746b8286ee63322cf2c45d2135359b85a7bb0
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-21 11:50:39 +1300

    tidied the function header

--- project_funcs/checkSetup_triggers.m
+++ project_funcs/checkSetup_triggers.m
@@ -1,8 +1,6 @@
 function [  ] = checkSetup_triggers(dataFolder, EGPGPath)
 %Checks to ensure that the trigger file has been created correctly, and
 %produces an error if any test fails.
-%   Checks that the file exists in the data folder
-%   Checks that the trigger values are not just the defaults
 %Inputs:    dataFolder = folder which scontains the users triggerName.txt file
 %           EGPGPath = folder which contains the default triggerNames.txt file
 
@@ -18,8 +16,7 @@ triggerNames = C{1,1};
 
 %If triggers haven't been edited from default, return error message
 if ~checkSetup_triggersEdited( triggerNames, EGPGPath )
-    error('It looks like you haven''t opened the triggerNames.txt file in your data folder and edited it to reflect the names of your triggers - see ''How to use'' in README.md')
+    error('It looks like you haven''t edited the triggerNames.txt file in your data folder to reflect the names of your triggers - see ''How to use'' in README.md')
 end
 
 end
-
