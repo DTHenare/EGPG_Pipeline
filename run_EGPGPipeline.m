f3c6a66c16b900286ea14dd7dc2a79224c2db650
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-20 14:27:13 +1300

    changed function name

---
+++ run_EGPGPipeline.m
@@ -0,0 +1,21 @@
+[selectedFile,dataFolder] = uigetfile({'*.*',  'All Files (*.*)'});
+if (dataFolder == 0) && (selectedFile == 0) 
+        error('Input file is not selected!') 
+end
+
+%Get path of pipeline folder
+EGPGPath = fileparts(mfilename('fullpath'));
+
+%Create list of files that will be processed
+[ fileNames,fileExt ] = getMatchingFiles(strcat(dataFolder, selectedFile));
+
+%Check setup is appropriate
+%checkSetup(dataFolder);
+
+%Preprocess each file in the data folder
+for i = 1:size(fileNames,1)
+    currentFile = strcat(dataFolder,fileNames{i,1},fileExt);
+    eeglab;
+    [ ALLEEG,EEG,CURRENTSET ] = EGPGPipeline(ALLEEG, EEG, CURRENTSET, currentFile, EGPGPath);
+    %clear eeglab data
+end
