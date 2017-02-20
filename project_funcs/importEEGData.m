8b6783e8512918ce4cf7d7a06bf4176213a2aec9
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-20 14:28:39 +1300

    created importEEGData function

---
+++ project_funcs/importEEGData.m
@@ -0,0 +1,26 @@
+function [ALLEEG,EEG,CURRENTSET] = importEEGData(ALLEEG, EEG, CURRENTSET, currentFile)
+%Imports a file into eeglab. Selects the appropriate load function based on
+%the file extension.
+%Inputs:    ALLEEG = ALLEEG structure produced by eeglab
+%           EEG = EEG structure produced by eeglab
+%           CURRENTSET = CURRENTSET value provided by eeglab
+%           currentFile = path of the EEG file which will be processed
+%Outputs:   ALLEEG = updated ALLEEG structure for eeglab
+%           EEG = updated EEG structure for eeglab
+%           CURRENTSET = updated CURRENTSET value for eeglab
+
+%Extract file parts
+[filePath, fileName, fileExt] = fileparts(currentFile);
+
+%Use relevant eeglab function to import data
+if strcmp('.RAW',fileExt)
+    EEG = pop_readegi(currentFile, [],[],'auto');
+    [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'gui','off');
+    [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+    EEG = eeg_checkset(EEG);
+elseif strcmp('.set',fileExt)
+    EEG = pop_loadset('filename',strcat(fileName,fileExt),'filepath',filePath);
+    [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+    EEG = eeg_checkset(EEG);
+end
+end
