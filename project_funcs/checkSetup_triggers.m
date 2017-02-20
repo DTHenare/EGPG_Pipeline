573e2cf63555952304f84dd724afd5e87886f9e9
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-21 11:38:26 +1300

    Added test for whether triggers file was edited

--- project_funcs/checkSetup_triggers.m
+++ project_funcs/checkSetup_triggers.m
@@ -1,6 +1,10 @@
-function [  ] = checkSetup_triggers(dataFolder)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [  ] = checkSetup_triggers(dataFolder, EGPGPath)
+%Checks to ensure that the trigger file has been created correctly, and
+%produces an error if any test fails.
+%   Checks that the file exists in the data folder
+%   Checks that the trigger values are not just the defaults
+%Inputs:    dataFolder = folder which scontains the users triggerName.txt file
+%           EGPGPath = folder which contains the default triggerNames.txt file
 
 %Does trigger file exist in the data folder?
 if exist(strcat(dataFolder,'\triggerNames.txt'), 'file') ~= 2
@@ -8,11 +12,14 @@ if exist(strcat(dataFolder,'\triggerNames.txt'), 'file') ~= 2
 end
 
 %Open text file and store contents in triggerNames
-FID = fopen(strcat(dataFolder,'\triggerNames.txt'))
+FID = fopen(strcat(dataFolder,'\triggerNames.txt'));
 C = textscan(FID, '%s');
 triggerNames = C{1,1};
 
-
+%If triggers haven't been edited from default, return error message
+if ~checkSetup_triggersEdited( triggerNames, EGPGPath )
+    error('It looks like you haven''t opened the triggerNames.txt file in your data folder and edited it to reflect the names of your triggers - see ''How to use'' in README.md')
+end
 
 end
 
