92d718ad68ce1c082e49bcbc9e665cd2b8b55b1d
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-20 14:26:47 +1300

    Added function header

--- project_funcs/getMatchingFiles.m
+++ project_funcs/getMatchingFiles.m
@@ -2,11 +2,10 @@ function [ fileNames,fileExt ] = getMatchingFiles( inputFile )
 %This function takes a single file as input, and returns a cell array which
 %contains the name of any file which shares the location and extension of
 %the input file.
-%Inputs:
-%   inputFile - Path of the file which will be matched
-%Outputs:
-%   fileNames - A cell array where each row contains the name of one file
-%   from the folder (with the extension removed)
+%Inputs:    inputFile = Path of the file which will be matched
+%Outputs:   fileNames = A cell array where each row contains the name of 
+%           one file from the folder (with the extension removed)
+%           fileExt = The extension of the input file
 
 %Identify file type from file name
 [filePath,fileName,fileExt] = fileparts(inputFile);
