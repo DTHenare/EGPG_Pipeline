b7ed68cd07162d703765aab95cf0eedba758534c
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-21 13:58:05 +1300

    Added a step to check for valid data file types

--- project_funcs/checkSetup.m
+++ project_funcs/checkSetup.m
@@ -1,4 +1,4 @@
-function [  ] = checkSetup(dataFolder, EGPGPath)
+function [  ] = checkSetup(dataFolder, EGPGPath, selectedFile)
 %Checks to ensure the the user has set up the data folder and trigger file
 %correctly in order to run the EGPG pipeline. Produces an error if anything
 %looks wrong.
@@ -12,7 +12,7 @@ checkSetup_parameters(EGPGPath);
 checkSetup_triggers(dataFolder, EGPGPath);
 
 %Check input file - extension appropriate
-
+checkSetup_datafile(selectedFile);
 
 end
 
