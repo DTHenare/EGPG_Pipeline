a3230fc60974ff3ffab2ca311056c903bb65d381
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-22 13:42:51 +1300

    Added epoching function

--- project_funcs/ERPPreprocess.m
+++ project_funcs/ERPPreprocess.m
@@ -13,19 +13,21 @@ function [ ALLEEG,EEG,CURRENTSET ] = ERPPreprocess(ALLEEG, EEG, CURRENTSET, curr
 load(strcat(EGPGPath,'\project_docs\Parameters.mat'));
 
 %Import data
-[ALLEEG,EEG,CURRENTSET] = importEEGData(ALLEEG, EEG, CURRENTSET, currentFile);
+[ALLEEG, EEG, CURRENTSET] = importEEGData( ALLEEG, EEG, CURRENTSET, currentFile );
 
 %Correct trigger latency
-[ALLEEG,EEG,CURRENTSET] = correctAmpDelay(ALLEEG, EEG, CURRENTSET);
+[ALLEEG, EEG, CURRENTSET] = correctAmpDelay( ALLEEG, EEG, CURRENTSET );
 
 %Load channel structure
 EEG=pop_chanedit(EEG, 'load',{strcat(EGPGPath,'\project_docs\GSN-HydroCel-129.sfp') 'filetype' 'autodetect'},'setref',{'4:132' 'Cz'},'changefield',{132 'datachan' 0});
 
 %High pass filter the data
-[ALLEEG, EEG, CURRENTSET] = EGPGFiltering(ALLEEG, EEG, CURRENTSET, EGPGPath, PARAMETERS.ERPHighpass, 1);
+[ALLEEG, EEG, CURRENTSET] = EGPGFiltering( ALLEEG, EEG, CURRENTSET, EGPGPath, PARAMETERS.ERPHighpass, 1 );
 
 %Average reference the data
 EEG = pop_reref( EEG, [],'refloc',struct('labels',{'Cz'},'Y',{0},'X',{5.4492e-16},'Z',{8.8992},'sph_theta',{0},'sph_phi',{90},'sph_radius',{8.8992},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{132},'datachan',{0}));
 
+%Epoch the events
+[ALLEEG, EEG, CURRENTSET] = epochEvents( ALLEEG, EEG, CURRENTSET,  PARAMETERS.epochMin, PARAMETERS.epochMax, currentFile );
 
 end
