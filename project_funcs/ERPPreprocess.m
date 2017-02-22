2b56301b0b38b7108bb9db0c47e807d1c7183375
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-22 13:15:22 +1300

    Added function for average referencing

--- project_funcs/ERPPreprocess.m
+++ project_funcs/ERPPreprocess.m
@@ -24,4 +24,8 @@ EEG=pop_chanedit(EEG, 'load',{strcat(EGPGPath,'\project_docs\GSN-HydroCel-129.sf
 %High pass filter the data
 [ALLEEG, EEG, CURRENTSET] = EGPGFiltering(ALLEEG, EEG, CURRENTSET, EGPGPath, PARAMETERS.ERPHighpass, 1);
 
+%Average reference the data
+EEG = pop_reref( EEG, [],'refloc',struct('labels',{'Cz'},'Y',{0},'X',{5.4492e-16},'Z',{8.8992},'sph_theta',{0},'sph_phi',{90},'sph_radius',{8.8992},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{132},'datachan',{0}));
+
+
 end
