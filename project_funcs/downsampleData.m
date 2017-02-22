c6af42adc11046038f194d7d6c71f4fe3dc22c4e
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-23 10:03:34 +1300

    Created downsampling function

---
+++ project_funcs/downsampleData.m
@@ -0,0 +1,19 @@
+function [ALLEEG, EEG, CURRENTSET] = downsampleData( ALLEEG, EEG, CURRENTSET, targetRate )
+%This function downsamples the data
+%Inputs:    ALLEEG = ALLEEG structure produced by eeglab
+%           EEG = EEG structure produced by eeglab
+%           CURRENTSET = CURRENTSET value provided by eeglab
+%           dwnsmplValue - This is the sampling frequency that the data
+%           will be changed to
+%Outputs:   ALLEEG = updated ALLEEG structure for eeglab
+%           EEG = updated EEG structure for eeglab
+%           CURRENTSET = updated CURRENTSET value for eeglab
+
+if targetRate < EEG.srate
+    EEG = pop_resample( EEG, targetRate);
+else
+    disp('Target sample rate is greater than or equal to the current sampling rate. Downsampling was not performed.')
+end
+
+end
+
