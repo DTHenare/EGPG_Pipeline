bb7e71cd0e4df59a0005e0fc06f338454d4785e4
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-24 12:48:30 +1300

    Fixed the function header

--- project_funcs/saveOutput.m
+++ project_funcs/saveOutput.m
@@ -1,12 +1,10 @@
-function [  ] = saveOutput(ALLEEG, EEG, CURRENTSET, saveLocation, saveName)
+function [ ALLEEG, EEG ] = saveOutput(ALLEEG, EEG, CURRENTSET, saveLocation, saveName)
 %
 %Inputs:    ALLEEG = ALLEEG structure produced by eeglab
 %           EEG = EEG structure produced by eeglab
 %           CURRENTSET = CURRENTSET value provided by eeglab
-
-%Outputs:   ALLEEG = updated ALLEEG structure for eeglab
-%           EEG = updated EEG structure for eeglab
-%           CURRENTSET = updated CURRENTSET value for eeglab
+%           saveLocation = file path where the data will be save
+%           saveName = filename that the data will be saved under
 
 EEG = pop_saveset( EEG, 'filename',strcat(saveName,'.set'),'filepath',saveLocation);
 [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
