d6cde7f563b497b8d811f1b23c1155d346132b20
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-23 13:35:57 +1300

    changed high pass filter order form 2 to 4, fixed function header

--- project_funcs/EGPGFiltering.m
+++ project_funcs/EGPGFiltering.m
@@ -3,17 +3,19 @@ function [ALLEEG, EEG, CURRENTSET] = EGPGFiltering(ALLEEG, EEG, CURRENTSET, filt
 %Inputs:    ALLEEG = ALLEEG structure produced by eeglab
 %           EEG = EEG structure produced by eeglab
 %           CURRENTSET = CURRENTSET value provided by eeglab
-%           EGPGPAth = path of the EGPG pipeline folder
+%           filterValue = filter frequency parameters. Note: for filter type
+%           is 1 or 2 this is a single value, for filter type 3 this is 2
+%           values
 %           filterType = Single value indicating which filter to run.
 %                       1 - Highpass only
 %                       2 - Lowpass only
-%                       3 - Both high and low pass
+%                       3 - Bandpass
 %Outputs:   ALLEEG = updated ALLEEG structure for eeglab
 %           EEG = updated EEG structure for eeglab
 %           CURRENTSET = updated CURRENTSET value for eeglab
 
 if filterType == 1
-    EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff',  filterValue, 'Design', 'butter', 'Filter', 'highpass', 'Order',  2, 'RemoveDC', 'on' );
+    EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff',  filterValue, 'Design', 'butter', 'Filter', 'highpass', 'Order',  4, 'RemoveDC', 'on' );
 elseif filterType == 2
     EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff',  filterValue, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  4, 'RemoveDC', 'on' );
 elseif filterType == 3
