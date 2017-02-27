e285cca6ac8a4c5aa7b3cf44906c5cebc0cc887d
DTHenare <dhen061@aucklanduni.ac.nz>
2017-02-28 11:34:06 +1300

    changed function used to extract conditions

--- project_funcs/extractConditions.m
+++ project_funcs/extractConditions.m
@@ -7,26 +7,24 @@ function [  ] = extractConditions(ALLEEG, EEG, CURRENTSET, currentFile)
 %           currentFile = path of the EEG file which will be processed
 
 %Get path information from current file
-[ filePath, fileName, fileExt ] = fileparts(currentFile);
+[ filePath, fileName ] = fileparts(currentFile);
 
 %Open the user triggerNames.txt file and store contents in triggerNames
 FID = fopen(strcat(filePath,'\triggerNames.txt'));
 C = textscan(FID, '%s');
 triggerNames = C{1,1};
 
-%create output folder if it doesn't already exist
-saveLocation = strcat(filePath,'\OutputConditions\');
-if ~exist(saveLocation, 'dir')
-  mkdir(saveLocation);
-end
+%Find paths for saving everything
+conditionsSaveLocation = strcat(filePath,'\OutputConditions\');
+wholeSaveLocation = strcat(filePath,'\Output\');
+saveName = strcat(fileName,'_Cleaned');
 
 for i = 1:length(triggerNames)
-    EEG = pop_epoch( EEG,  triggerNames(i) , [EEG.xmin, EEG.xmax], 'newname', strcat(fileName,'-',triggerNames{i}), 'epochinfo', 'yes');
-    [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',strcat(saveLocation,fileName,'-',triggerNames{i},'.set'),'gui','off');
-    EEG = eeg_checkset( EEG );
-    [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET,'retrieve',1,'study',0);
-    EEG = eeg_checkset( EEG );
+    
+    EEG = pop_rmdat( EEG, {triggerNames(i)},[EEG.xmin EEG.xmax] ,0);
+    [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',strcat(conditionsSaveLocation,fileName,'-',triggerNames{i},'.set'),'saveold',strcat(wholeSaveLocation,saveName,'.set'),'gui','off');
+    [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
+    
 end
 
 end
-
