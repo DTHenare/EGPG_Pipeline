ff2166ba262271edfb9b2cd52912a33246ae4cb3
Dion Henare <dionhenare@gmail.com>
2017-02-27 20:34:30 +1300

    fixed typo

--- project_funcs/createOutputObjects.m
+++ project_funcs/createOutputObjects.m
@@ -25,7 +25,7 @@ if ~exist(outputInvInfo, 'file')
    numGenFails = [];
    meanHEOG = [];
    IndividualInfo = struct('badChannels',badChannels, 'epochNum',epochNum, 'horizFails',horizFails, 'numGenFails',numGenFails, 'meanHEOg',meanHEOG);
-   save(strcat(outputLocation,'IndividualInfo.mat'),'IndividiualInfo')
+   save(strcat(outputLocation,'IndividualInfo.mat'),'IndividualInfo')
 end
 
 end
