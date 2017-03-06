e744414682c9ba416590d6882eaf8c26bc6bd77a
Dion Henare <dionhenare@gmail.com>
2017-03-06 21:12:43 +1300

    added support for multiple segmented netstation files, only saves the first name of a segmented set to filenames

--- project_funcs/getMatchingFiles.m
+++ project_funcs/getMatchingFiles.m
@@ -1,9 +1,9 @@
-function [ fileNames,fileExt ] = getMatchingFiles( inputFile )
+function [ fileNames,fileExt,segPresent ] = getMatchingFiles( inputFile )
 %This function takes a single file as input, and returns a cell array which
 %contains the name of any file which shares the location and extension of
 %the input file.
 %Inputs:    inputFile = Path of the file which will be matched
-%Outputs:   fileNames = A cell array where each row contains the name of 
+%Outputs:   fileNames = A cell array where each row contains the name of
 %           one file from the folder (with the extension removed)
 %           fileExt = The extension of the input file
 
@@ -12,13 +12,36 @@ function [ fileNames,fileExt ] = getMatchingFiles( inputFile )
 
 %Get properties of all matching files in the folder
 allMatches = dir(strcat(filePath,'\*',fileExt));
+
 %Make fileNum equal the number of matching files found in the folder
 fileNum = size(allMatches,1);
-
-%Store only the file names (excluding the extension) in fileNames
+%store length of extension
 extLength = size(fileExt,2);
-for i = 1:fileNum
-    fileNames{i,1} = allMatches(i,1).name(1:end-extLength);
+%assume non-segmented files
+segPresent = 0;
+
+%Always store the first file in finalNames
+fileNames{1,1} = allMatches(1,1).name(1:end-extLength);
+j=2;
+%for all other files, only store them if they don't look like segments of
+%the previous file
+for i = 2:fileNum
+    if strcmp(allMatches(i,1).name(1:end-7),allMatches(i-1,1).name(1:end-7)) && strcmp(allMatches(i,1).date(1:end-5),allMatches(i-1,1).date(1:end-5))
+        segPresent = 1;
+    else
+        fileNames{j,1} = allMatches(i,1).name(1:end-extLength);
+        j=j+1;
+    end
+end
+
+%check that something weird hasn't happened. If files are segmented, then
+%all of the fileNames should end in 1 otherwise they won't load
+if segPresent == 1
+    for i = 1:length(fileNames)
+        if fileNames{1}(end) ~= 1
+            error('The files in your data folder look like they''re segmented netsstation files, but not all participants have a file ending in 1')
+        end
+    end
 end
 
 end
