feb0bdd729ef831dc6b20a93478cd22565689de9
Dion Henare <dionhenare@gmail.com>
2017-03-07 22:55:28 +1300

    updated the createStudy line so it works with finished createStudy function

--- run_EGPGPipeline.m
+++ run_EGPGPipeline.m
@@ -37,4 +37,5 @@ for i = 1:size(fileNames,1)
     STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
 end
 
-[ STUDY, ALLEEG, EEG, CURRENTSET ] = createStudySet(STUDY, ALLEEG, EEG, CURRENTSET, triggerNames, fileNames, dataFolder );
+%Create the study!
+createStudySet(STUDY, ALLEEG, EEG, CURRENTSET, triggerNames, fileNames, dataFolder );
