b157fd7b55c6bbbf593fbe1e38fdfd9ee40dd992
Dion Henare <dionhenare@gmail.com>
2017-03-08 20:07:01 +1300

    added inputs for the min and max values so it can be used more generally

--- project_funcs/identExtremeValues.m
+++ project_funcs/identExtremeValues.m
@@ -1,4 +1,4 @@
-function [ extremFails ] = identExtremeValues( EEG )
+function [ extremFails ] = identExtremeValues( EEG, minVoltage, maxVoltage )
 %Identifies epochs which have extreme values (+/- 100 microvolts)
 %Inputs:    ALLEEG = ALLEEG structure produced by eeglab
 %           EEG = EEG structure produced by eeglab
@@ -13,7 +13,7 @@ blMin = EEG.xmin * 1000;
 EEG = pop_rmbase( EEG, [blMin 0]);
 
 %Identify artifact epochs
-[EEG, extremFails] = pop_eegthresh(EEG,1,[1:EEG.nbchan] ,-100,100,EEG.xmin,EEG.xmax,0,0);
+[EEG, extremFails] = pop_eegthresh(EEG,1,[1:EEG.nbchan] ,minVoltage,maxVoltage,EEG.xmin,EEG.xmax,0,0);
 
 end
 
