18968b9db69209b62cf142a67eda6a5539890feb
Dion Henare <dionhenare@gmail.com>
2017-03-16 11:46:25 +1300

    added catches in case erplab or firfilt aren't installed

--- project_funcs/EGPGFiltering.m
+++ project_funcs/EGPGFiltering.m
@@ -1,5 +1,7 @@
 function [ALLEEG, EEG, CURRENTSET] = EGPGFiltering(ALLEEG, EEG, CURRENTSET, filterValue, filterType)
-%Filters eeglab data using the ERPLab filter function.
+%Filters eeglab data using the ERPLab filter function. If ERPLab filter
+%doesn't work, it trys to use the new eeglab filter. If that doesn't work,
+%it uses the legacy eeglab filter.
 %Inputs:    ALLEEG = ALLEEG structure produced by eeglab
 %           EEG = EEG structure produced by eeglab
 %           CURRENTSET = CURRENTSET value provided by eeglab
@@ -19,7 +21,15 @@ if filterType == 1
 elseif filterType == 2
     EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff',  filterValue, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  4, 'RemoveDC', 'on' );
 elseif filterType == 3
-    EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff', filterValue, 'Design', 'butter', 'Filter', 'bandpass', 'Order',  4, 'RemoveDC', 'on' );
+    try
+        EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff', filterValue, 'Design', 'butter', 'Filter', 'bandpass', 'Order',  4, 'RemoveDC', 'on' );
+    catch
+        try
+            EEG = pop_eegfiltnew(EEG, 0.1, 30);
+        catch
+            EEG = pop_eegfilt( EEG, 0.1, 30, [], [0], 0, 0, 'fir1', 0);
+        end
+    end
 else
     error('The filterType you selected is not valid. It must be either 1, 2, or 3.')
 end
