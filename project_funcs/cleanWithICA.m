5fea53a7d80a4bef27c36084f9cc1848e273ff07
Dion Henare <dionhenare@gmail.com>
2017-06-29 18:11:42 +1200

    Included lines that actually remove components identified by ADJUST

--- project_funcs/cleanWithICA.m
+++ project_funcs/cleanWithICA.m
@@ -12,43 +12,43 @@ EEG.icasphere = ICAStruct.icasphere;
 EEG.icachansind = ICAStruct.icachansind;
 
 try
-%% Fit Dipoles for components
-%C:\Program Files\MATLAB\R2013a\toolbox\eeglab13_1_1b\plugins\dipfit2.2\standard_BESA\
-EEG = pop_dipfit_settings( EEG, 'hdmfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard_BESA.mat','coordformat','Spherical','mrifile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\avg152t1.mat','chanfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard-10-5-cap385.elp','coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:128] );
-[ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
-EEG = pop_multifit(EEG,[],'threshold',100,'plotopt',{'normlen' 'on'});
-[ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
-
-%% Remove poorly fitted components (high residual variance dipoles)
-rvFails=[];
-rvThresh=15;
-for dipNum=1:size(EEG.icaweights,1)
-    if EEG.dipfit.model(1,dipNum).rv > (rvThresh/100)
-        rvFails = [rvFails dipNum];
+    %% Fit Dipoles for components
+    %C:\Program Files\MATLAB\R2013a\toolbox\eeglab13_1_1b\plugins\dipfit2.2\standard_BESA\
+    EEG = pop_dipfit_settings( EEG, 'hdmfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard_BESA.mat','coordformat','Spherical','mrifile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\avg152t1.mat','chanfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard-10-5-cap385.elp','coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:128] );
+    [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
+    EEG = pop_multifit(EEG,[],'threshold',100,'plotopt',{'normlen' 'on'});
+    [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
+    
+    %% Remove poorly fitted components (high residual variance dipoles)
+    rvFails=[];
+    rvThresh=15;
+    for dipNum=1:size(EEG.icaweights,1)
+        if EEG.dipfit.model(1,dipNum).rv > (rvThresh/100)
+            rvFails = [rvFails dipNum];
+        end
     end
-end
-%Remove identified components
-if ~isempty(rvFails)
-    EEG = pop_subcomp( EEG, rvFails, 0);
-end
-
-%% Remove components with dipoles fitted outside of the head
-locFails=[];
-for dipNum=1:size(EEG.icaact,1)
-    if any(sqrt(sum(EEG.dipfit.model(dipNum).posxyz.^2,2)) > 85)
-        locFails = [ locFails dipNum];
+    %Remove identified components
+    if ~isempty(rvFails)
+        EEG = pop_subcomp( EEG, rvFails, 0);
     end
-end
-%Remove identified components
-if ~isempty(locFails)
-    EEG = pop_subcomp( EEG, locFails, 0);
-end
-
-
-%% Output number of failed components
-rvSize = size(rvFails,2);
-locSize = size(locFails,2);
-totalNumberOfFails = rvSize+locSize;
+    
+    %% Remove components with dipoles fitted outside of the head
+    locFails=[];
+    for dipNum=1:size(EEG.icaact,1)
+        if any(sqrt(sum(EEG.dipfit.model(dipNum).posxyz.^2,2)) > 85)
+            locFails = [ locFails dipNum];
+        end
+    end
+    %Remove identified components
+    if ~isempty(locFails)
+        EEG = pop_subcomp( EEG, locFails, 0);
+    end
+    
+    
+    %% Output number of failed components
+    rvSize = size(rvFails,2);
+    locSize = size(locFails,2);
+    totalNumberOfFails = rvSize+locSize;
 catch
     totalNumberOfFails=[];
 end
@@ -59,6 +59,9 @@ try
     saveAdjust = strcat(folderPath,'\Output\ProcessingInfo\',fileName,'.txt');
     [ ADJUSTarts ] = ADJUST(EEG,saveAdjust);
     numADJUSTFails = length(ADJUSTarts);
+    if ~isempty(ADJUSTarts)
+        EEG = pop_subcomp( EEG, ADJUSTarts, 0);
+    end
 catch
     ADJUSTarts = [];
     numADJUSTFails = 0;
