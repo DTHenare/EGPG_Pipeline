81db8f695f1b787a5152577941d0189d629554da
Dion Henare <dionhenare@gmail.com>
2017-06-29 15:12:00 +1200

    Fieldtrip broken, added try/catch to avoid error when dipole fitting fails

--- project_funcs/cleanWithICA.m
+++ project_funcs/cleanWithICA.m
@@ -8,12 +8,13 @@ EEG.icawinv = ICAStruct.icawinv;
 EEG.icasphere = ICAStruct.icasphere;
 EEG.icachansind = ICAStruct.icachansind;
 
+try
 %% Fit Dipoles for components
 %C:\Program Files\MATLAB\R2013a\toolbox\eeglab13_1_1b\plugins\dipfit2.2\standard_BESA\
-EEG = pop_dipfit_settings( EEG, 'hdmfile','standard_BESA.mat','coordformat','Spherical','mrifile','avg152t1.mat','chanfile','standard-10-5-cap385.elp','coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:EEG.nbchan] );
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, 0);
+EEG = pop_dipfit_settings( EEG, 'hdmfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard_BESA.mat','coordformat','Spherical','mrifile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\avg152t1.mat','chanfile','C:\\Program Files\\MATLAB\\R2013a\\toolbox\\eeglab13_1_1b\\plugins\\dipfit2.2\\standard_BESA\\standard-10-5-cap385.elp','coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:128] );
+[ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
 EEG = pop_multifit(EEG,[],'threshold',100,'plotopt',{'normlen' 'on'});
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, 0);
+[ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
 
 %% Remove poorly fitted components (high residual variance dipoles)
 rvFails=[];
@@ -45,5 +46,8 @@ end
 rvSize = size(rvFails,2);
 locSize = size(locFails,2);
 totalNumberOfFails = rvSize+locSize;
+catch
+    totalNumberOfFails=[];
+end
 end
 
