83e79018e4c48ed46bf08ae44807698717991a5d
Dion Henare <dionhenare@gmail.com>
2017-07-02 20:09:07 +1200

    Plots GFP with peaks marked, and topos of peak times

--- project_funcs/createStudyOutput.m
+++ project_funcs/createStudyOutput.m
@@ -11,9 +11,6 @@ function [  ] = createStudyOutput( STUDY, ALLEEG, triggerNames, acceptedFiles, d
 %           blMin = time in seconds that the prestimulus period begins
 %           relative to the event trigger.
 
-%Create list of channels
-channelList = {STUDY.changrp(:).name};
-
 %Make study design which combines all conditions
 STUDY = std_makedesign(STUDY, ALLEEG, 2, 'variable1','condition','variable2','','name','GrandAverage','pairing1','on','pairing2','on','delfiles','off','defaultdesign','off','values1',{triggerNames'},'subjselect',acceptedFiles);
 
@@ -21,21 +18,59 @@ STUDY = std_makedesign(STUDY, ALLEEG, 2, 'variable1','condition','variable2','',
 [STUDY, ALLEEG] = std_precomp(STUDY, ALLEEG, {},'interp','on','recompute','on','erp','on','erpparams',{'rmbase' [(blMin*1000) 0] });
 CURRENTSTUDY = 1; EEG = ALLEEG; CURRENTSET = [1:length(EEG)];
 
+%Grab some parameters from the EEG data
+channelList = {STUDY.changrp(:).name};
+chanLocs = EEG(1).chanlocs;
+epochEnd = EEG(1).xmax*1000;
+postStimX = 0:(1000/EEG(1).srate):epochEnd;
+postStimLength = length(postStimX);
+
 %Plot all electrodes on the scalp
 [ STUDY, allData, erpTimes ] = std_erpplot(STUDY,ALLEEG,'channels',channelList);
 CURRENTSTUDY = 1; EEG = ALLEEG; CURRENTSET = [1:length(EEG)];
 %Save the figure into the output folder
-saveas(gcf,strcat(dataFolder,'Output\GrandAveragePlot.fig'));
+saveas(gcf,strcat(dataFolder,'Output\StudyPlots\GrandAveragePlot.fig'));
 
 %Calculate the global field power
 participantCollapsed = mean(allData{1,1},3);
 gfp=std(participantCollapsed,0,2);
-%Plot the global field power
-figure;plot(gfp)
+
+%Extract peak values, locations, widths, and prominence for poststim GFP
+[peaks,locs,w,p]=findpeaks(gfp(end-postStimLength-1:end));
+%Sort by prominence
+[sortedP, sortIndP ]= sort(p, 'descend');
+%plot 5 most prominent peaks on GFP
+figure;subplot(2,5,2:4)
+findpeaks(gfp(end-postStimLength-1:end),postStimX,'MinPeakProminence',sortedP(5),'Annotate','extents')
+
+%Extract location of the 5 most prominent peaks and their widths
+top5Locs = locs(sortIndP(1:5))*(1000/EEG(1).srate);
+top5Widths = w(sortIndP(1:5));
+%flip to ascending order
+top5Locs = flipud(top5Locs);
+top5Widths = flipud(top5Widths);
+
+for i = 1:5
+    %Get time window for topography from the peaks
+    topoTimePoint = top5Locs(i)+(blMin*1000);
+    topoHalfWidth = floor(top5Widths(i)/2);
+    
+    %Set parameters for plotting topography
+    STUDY = pop_erpparams(STUDY, 'topotime',[topoTimePoint-topoHalfWidth, topoTimePoint+topoHalfWidth] );
+    %Extract topo values
+    [STUDY, curTopoData] = std_erpplot(STUDY,ALLEEG,'noplot','on','channels',channelList);
+    %Average across participants
+    curTopoData= mean(curTopoData{1,1},3);
+    %plot topography on subplot
+    subplot(2,5,i+5)
+    topoplot(curTopoData, chanLocs);
+    title(strcat('Peak-',int2str(i)));
+end
+
 %Save GFP data
-save(strcat(dataFolder,'Output\GFP.mat'),'gfp');
+save(strcat(dataFolder,'Output\ProcessingInfo\GFP.mat'),'gfp');
 %Save the figure to the output folder
-saveas(gcf,strcat(dataFolder,'Output\GlobalFieldPower.fig'));
+saveas(gcf,strcat(dataFolder,'Output\StudyPlots\GFP-guided-topos.fig'));
 
 end
 
