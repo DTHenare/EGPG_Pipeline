6c2785d7ab71abcfeb3f8e051a6d94d8aa195088
Dion Henare <dionhenare@gmail.com>
2017-07-04 18:40:22 +1200

    changed back to script

--- run_EGPGPipeline.m
+++ run_EGPGPipeline.m
@@ -1,4 +1,3 @@
-function run_EGPGPipeline()
 %This script provides a user friendly wrapper for the EGPG pipeline. When
 %run, it pops open a file explorer for the user to navigate to a data file.
 %It then runs the pipeilne for all matching files within the selected
@@ -48,4 +47,3 @@ end
 %Create the study!
 [ failedFiles ] = createStudySet(STUDY, ALLEEG, EEG, CURRENTSET, triggerNames, fileNames, dataFolder, EGPGPath );
 save(strcat(dataFolder,'/Output/ProcessingInfo/participantsExcludedFromSTUDY.mat'),'failedFiles');
-end
