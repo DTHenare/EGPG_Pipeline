fba3efc9c4343159156b61e8099cff1d552eb319
Dion Henare <dionhenare@gmail.com>
2017-07-04 18:27:57 +1200

    removed precompute (now performed earlier)

--- project_funcs/createOutputSheet.m
+++ project_funcs/createOutputSheet.m
@@ -1,9 +1,9 @@
-function [ Output ] = createOutputSheet( STUDY, ALLEEG, blMin, channelList, conditions )
+function [ Output ] = createOutputSheet( STUDY, ALLEEG, channelList, conditions )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-[STUDY ALLEEG] = std_precomp(STUDY, ALLEEG, {},'interp','on','recompute','on','erp','on','erpparams',{'rmbase' [blMin 0] });
-[ STUDY, Output.allData, Output.erpTimes ] = std_erpplot(STUDY,ALLEEG,'channels',channelList);
+%[STUDY ALLEEG] = std_precomp(STUDY, ALLEEG, {},'interp','on','recompute','on','erp','on','erpparams',{'rmbase' [blMin 0] });
+[ ~, Output.allData, Output.erpTimes ] = std_erpplot(STUDY,ALLEEG,'channels',channelList);
 close(gcf);
 
 %Average across participants
