95b9e402529476f79baf776fc63c78fbec46a3bf
DTHenare <dionhenare@gmail.com>
2017-07-05 13:38:18 +1200

    Added function header

--- project_funcs/getElectrodePairs.m
+++ project_funcs/getElectrodePairs.m
@@ -1,6 +1,11 @@
 function [ electrodePairs ]= getElectrodePairs(chanlocs, numElectrodes)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%Produces a list of electrode pairs that are the left/right hemisphere
+%homologues
+%Inputs:    chanlocs = chanlocs structure taken from eeglab
+%           numElectrodes the total number of electrodes which are not on
+%           the midline
+%Outputs:   electrodePairs = A matrix holding each electrode and its
+%           homologous paired electrode
 
 index = 1;
 for i = 1:numElectrodes
