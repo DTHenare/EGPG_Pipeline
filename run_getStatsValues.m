c44b9c4fe194eec34ea05d927a75c1676c7b2c26
DTHenare <dionhenare@gmail.com>
2017-12-20 17:06:08 +1300

    added commenting for plot

--- run_getStatsValues.m
+++ run_getStatsValues.m
@@ -67,16 +67,21 @@ for cond = 1:numCond
     ERPhand(cond) = plot(xAxis,mean(data{cond}(:,elec,:),3),'linewidth', 2);
     hold on
 end
+%reapply automatic axis limits
 xlim auto
 ylim auto
+%extract automatic axis limits
 xl = xlim;
 yl = ylim;
+%draw axis lines and line marking component of interest
 h1 = line([compLatencyms compLatencyms], [yl(1) yl(2)], 'LineWidth', compWidth, 'Color', [0.75 0.75 0.75]);
 h2 = line([0 0], [yl(1) yl(2)], 'LineWidth', 2, 'Color', [0 0 0]);
 h3 = line([xl(1) xl(2)], [0 0], 'LineWidth', 2, 'Color', [0 0 0]);
+%add title, axis labels, and legend
 title('ERPs')
 xlabel('Time(ms)')
 ylabel('Amplitude(µV)','linewidth', 2)
 legend(ERPhand,conditions,'Location', 'southeast')
+%rearrange line stacking order so that ERPs are in front
 uistack([h2 h3], 'bottom')
 uistack(h1, 'bottom')
