e1d2716a421ed66402da40506a19a9897bd8191b
DTHenare <dionhenare@gmail.com>
2017-12-22 19:02:02 +1300

    created function

---
+++ project_funcs/convertMsToSamp.m
@@ -0,0 +1,15 @@
+function [ samplepoint ] = convertMsToSamp( timepoint, baseline, samplingFreq )
+%Converts a time point in ms into a sample point ie. the location of that 
+%time point in an array 
+%Inputs:    timepoint = The time in ms that will be converted.
+%           baseline = The start of the baseline period in ms
+%           samplingFreq = The current sampling frequency of the data
+%Outputs:   samplepoint = the sample point that corresponds to the
+%t          imepoint inputted
+
+samplingRate = 1000/samplingFreq;
+samplepoint = (timepoint + abs(baseline))/samplingRate;
+samplepoint = round(samplepoint);
+
+end
+
