c91bb8990cb6745d12dfa7fdec9897847e68fcde
Dion Henare <dionhenare@gmail.com>
2018-01-25 18:53:52 +1300

    rewrote script for simplicity

--- project_funcs/createTopoPlot.m
+++ project_funcs/createTopoPlot.m
@@ -8,28 +8,35 @@ figure;
 minY = 0;
 maxY = 0;
 
+%Plot each set of data to get info about y limits etc.
+for cond = 1:numCond
+    plotData = mean(data{cond}(:,elec,:),2);
+    h = figure;
+    plot(xAxis,mean(plotData(:,:,:),3),'linewidth', 2);
+    
+    yl = ylim;
+    minY = min(minY,yl(1));
+    maxY = max(maxY,yl(2));
+    close(h)
+end
+
 for cond = 1:numCond
     ax(cond) = subplot(2,numCond,cond+numCond);
     plotData = mean(data{cond}(:,elec,:),2);
     ERPhand(cond) = plot(xAxis,mean(plotData(:,:,:),3),'linewidth', 2);
     
     %reapply automatic axis limits
-    xlim([-200,800])
-    ylim auto
-    %extract automatic axis limits
-    xl = xlim;
-    yl = ylim;
-    
-    ylim(ax(cond),[yl(1),yl(2)])
+    xlim(ax(cond),[-200,800])
+    ylim(ax(cond),[minY,maxY])
     recX = compLatencyms - (compWidth/2);
-    recY = yl(1);
+    recY = minY;
     recWidth = compWidth;
-    recHeight = abs(yl(1))+abs(yl(2));
+    recHeight = abs(minY)+abs(maxY);
     
     %draw axis lines and line marking component of interest
-    r(cond) = rectangle('Position', [ recX recY recWidth recHeight ], 'EdgeColor', [0.75 0.75 0.75], 'FaceColor', [0.75 0.75 0.75] );
-    l1(cond) = line([0 0], [yl(1) yl(2)], 'LineWidth', 2, 'Color', [0 0 0]);
-    l2(cond) = line([xl(1) xl(2)], [0 0], 'LineWidth', 2, 'Color', [0 0 0]);
+    r = rectangle('Position', [ recX recY recWidth recHeight ], 'EdgeColor', [0.75 0.75 0.75], 'FaceColor', [0.75 0.75 0.75] );
+    yline = line([0 0], [minY maxY], 'LineWidth', 2, 'Color', [0 0 0]);
+    xline = line([-200 800], [0 0], 'LineWidth', 2, 'Color', [0 0 0]);
     
     %add title, axis labels, and legend
     title('ERPs')
@@ -37,24 +44,8 @@ for cond = 1:numCond
     ylabel('Amplitude(µV)','linewidth', 2)
     %legend(ERPhand,conditions,'Location', 'southeast')
     %rearrange line stacking order so that ERPs are in front
-    uistack([l1(cond) l2(cond)], 'bottom')
-    uistack(r(cond), 'bottom')
-    
-    minY = min(minY,yl(1));
-    maxY = max(maxY,yl(2));
-    
-end
-
-for cond = 1:numCond
-    ylim(ax(cond),[minY,maxY])
-    recX = compLatencyms - (compWidth/2);
-    recY = minY;
-    recWidth = compWidth;
-    recHeight = abs(minY)+abs(maxY);
-    
-    rectangle(r(cond),'Position', [ recX recY recWidth recHeight ], 'EdgeColor', [0.75 0.75 0.75], 'FaceColor', [0.75 0.75 0.75] );
-    line(l1(cond), [0 0], [minY maxY], 'LineWidth', 2, 'Color', [0 0 0]);
-    line(l2(cond),[xl(1) xl(2)], [0 0], 'LineWidth', 2, 'Color', [0 0 0]);
+    uistack([yline xline], 'bottom')
+    uistack(r, 'bottom')    
 end
 
 end
