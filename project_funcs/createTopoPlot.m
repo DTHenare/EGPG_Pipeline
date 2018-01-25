c76f88f791eb4cffe408ae1ebfa607a754f92754
DTHenare <dionhenare@gmail.com>
2018-01-26 11:42:41 +1300

    added grand average topoplot

--- project_funcs/createTopoPlot.m
+++ project_funcs/createTopoPlot.m
@@ -1,6 +1,9 @@
-function [  ] = createTopoPlot( conditions, data, xAxis, elec, compLatencyms, compWidth, chanlocs )
+function [  ] = createTopoPlot( conditions, data, xAxis, elec, compLatencyms, compWidth, compWinMin, compWinMax, chanlocs )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+%Start eeglab
+eeglab;
 
 numCond = length(conditions);
 figure;
@@ -20,6 +23,7 @@ for cond = 1:numCond
     close(h)
 end
 
+%Plot the ERPs
 for cond = 1:numCond
     ax(cond) = subplot(2,numCond,cond+numCond);
     plotData = mean(data{cond}(:,elec,:),2);
@@ -39,7 +43,7 @@ for cond = 1:numCond
     xline = line([-200 800], [0 0], 'LineWidth', 2, 'Color', [0 0 0]);
     
     %add title, axis labels, and legend
-    title('ERPs')
+    title(conditions(cond))
     xlabel('Time(ms)')
     ylabel('Amplitude(µV)','linewidth', 2)
     %legend(ERPhand,conditions,'Location', 'southeast')
@@ -47,6 +51,28 @@ for cond = 1:numCond
     uistack([yline xline], 'bottom')
     uistack(r, 'bottom')    
 end
+
+%Create topo  plot data
+for cond = 1:numCond
+    curData = mean(data{cond}(compWinMin:compWinMax,:,:),1);
+    curData = mean(curData,3);
+    topoData(cond,:) = curData;
+end
+
+%get the min and max value of the topo data for plotting purposes
+topoMin = mean(topoData(topoData==min(topoData(:))));
+topoMax = mean(topoData(topoData==max(topoData(:))));
+
+%Plot the topographies
+for cond = 1:numCond
+    subplot(2,numCond, cond)
+    topoplot( topoData(cond,:), chanlocs, 'electrodes', 'ptslabels', 'maplimits', [ topoMin, topoMax ] );
+end
+
+%Plot grand average topo
+figure
+grandTopo = mean(topoData,1);
+topoplot( grandTopo, chanlocs, 'electrodes', 'ptslabels', 'maplimits', [ topoMin, topoMax ] );
 
 end
 
