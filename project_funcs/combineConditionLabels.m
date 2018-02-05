c666cc57009fe2a2726cfcd4d5856baf72de77cd
DTHenare <dionhenare@gmail.com>
2018-02-06 00:06:40 +1300

    moved parts out to higher function and fixed bugs

--- project_funcs/combineConditionLabels.m
+++ project_funcs/combineConditionLabels.m
@@ -1,33 +1,30 @@
-function [ newCell ]  = combineConditionLabels( dataFolder, dataFile, newLabel, combineLabels )
+function [ dataStruct ]  = combineConditionLabels( dataStruct, newLabel, combineLabels )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-%Load the file
-load(strcat(dataFolder,dataFile));
-
-newData  = Output.allData;
-totalConds = length(Output.conditions);
+erpData  = dataStruct.allData;
+totalConds = length(dataStruct.conditions);
 condLocs = zeros(totalConds,1);
 
 %Find cell locations which match the conditions we're combining
 for curLabel = 1:length(combineLabels)
-    condLocs = condLocs + strcmp(Output.conditions,combineLabels{curLabel});
+    condLocs = condLocs + strcmp(dataStruct.conditions,combineLabels{curLabel});
 end
 condLocs = logical(condLocs);
 %Create 4D array from the cells holding the data we'll combine
-matchingConditions = cat(4,newData{condLocs});
+matchingConditions = cat(4,erpData{condLocs});
 
 %average across the conditions
 newCell = mean(matchingConditions,4);
 
 %Create new output data remove the combined cells and add the new data
-nonMatchingConditions = ~matchingConditions;
-newOutput = newData(nonMatchingConditions);
+nonMatchingConditions = ~condLocs;
+newOutput = erpData(nonMatchingConditions);
 newOutput{end+1} = newCell;
-Output.allData = newOutput;
+dataStruct.allData = newOutput;
 
 %Remove the labels of the combined conditions, and add new label
-Output.conditions(nonMatchingConditions);
-Output.conditions{end+1} = newLabel;
+dataStruct.conditions = dataStruct.conditions(nonMatchingConditions);
+dataStruct.conditions{end+1} = newLabel{1};
 
 end
