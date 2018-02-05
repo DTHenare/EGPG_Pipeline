5a21990b7fc68dd31526bf8fffe10612dacd4e64
DTHenare <dionhenare@gmail.com>
2018-02-06 00:07:20 +1300

    added while loop so user can repeat for as many times as necessary

--- run_combineConditionLabels.m
+++ run_combineConditionLabels.m
@@ -3,13 +3,28 @@
 if (dataFolder == 0) & (dataFile == 0)
     error('Input file is not selected!')
 end
+%Load the file
+load(strcat(dataFolder,dataFile));
+userDecision = 1;
+curData = Output;
 
-%Ask the user which events will be combined
-[eventSelection, OK] = listdlg('promptstring','Select the events that you''d like to combine','ListString',Output.conditions);
-if OK==0
-    error('you must select at least one event')
+while userDecision == 1
+    %Ask the user which events will be combined
+    [eventSelection, OK] = listdlg('promptstring','Select the events that you''d like to combine','ListString',curData.conditions);
+    if OK==0
+        error('You must select at least one event!')
+    end
+    combineLabels = curData.conditions(eventSelection);
+    
+    %Ask user for the new label name
+    newLabel = inputdlg('Enter the new label name:' );
+    
+    %combine the data and return the new data set
+    curData = combineConditionLabels( curData, newLabel, combineLabels );
+    
+    %ask if the user wants to do another combination
+    userDecision = menu('Done! Would you like to combine another set of labels?','Yes','No');
 end
 
-%Ask user for the new label name
-newLabel = inputdlg('Enter the new label name:' );
-
+%Save the new data set
+save([dataFolder 'combined' dataFile],'curData')
