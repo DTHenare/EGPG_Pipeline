ec16db69b9c15e7cfb5bcabaf746ea8f59f23cbc
DTHenare <dionhenare@gmail.com>
2018-10-29 16:34:07 +1300

    removed EEG output

--- project_funcs/detectBadChannels.m
+++ project_funcs/detectBadChannels.m
@@ -1,4 +1,4 @@
-function [ badChannels, EEG ] = detectBadChannels( EEG )
+function [ badChannels] = detectBadChannels( EEG )
 %Detects bad channels in the data using a range of tests and outputs the
 %names of the bad channels.
 %Inputs:    EEG = EEG structure produced by eeglab
@@ -9,7 +9,7 @@ function [ badChannels, EEG ] = detectBadChannels( EEG )
 tempLocs = EEG.chanlocs;
 
 %Reject channels with abnormal spectrum
-[EEG, specFails] = pop_rejchanspec( EEG, 'freqlims', [0 35], 'stdthresh', [-15 3], 'plotchans', 'off');
+[~, specFails] = pop_rejchanspec( EEG, 'freqlims', [0 35], 'stdthresh', [-15 3], 'plotchans', 'off');
 
 %Create single list of all failed channels
 %allFails = union(specFails,otherThingsWhichIWillHaveOneDay)
