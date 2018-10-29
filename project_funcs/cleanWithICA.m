2d77ccd62e773e64db6a0a36decaa4e169b6f789
DTHenare <dionhenare@gmail.com>
2018-10-29 14:23:44 +1300

    added interpolation to catch to prevent bug

--- project_funcs/cleanWithICA.m
+++ project_funcs/cleanWithICA.m
@@ -25,11 +25,11 @@ appendMethods(fid, [' ICA weights were then copied over to this set of data in o
 
 if false
     %% Fit Dipoles for components
-        dipfitPath = fileparts(which('pop_dipfit_settings'))
-        EEG = pop_dipfit_settings( EEG, 'hdmfile',[dipfitPath '\\standard_BESA\\standard_BESA.mat'],'coordformat','Spherical','mrifile',[dipfitPath '\\standard_BESA\\avg152t1.mat'],'chanfile',[dipfitPath '\\standard_BESA\\standard-10-5-cap385.elp'],'coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:EEG.nbchan] );
-        [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
-        EEG = pop_multifit(EEG,[],'threshold',100,'plotopt',{'normlen' 'on'});
-        [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
+    dipfitPath = fileparts(which('pop_dipfit_settings'))
+    EEG = pop_dipfit_settings( EEG, 'hdmfile',[dipfitPath '\\standard_BESA\\standard_BESA.mat'],'coordformat','Spherical','mrifile',[dipfitPath '\\standard_BESA\\avg152t1.mat'],'chanfile',[dipfitPath '\\standard_BESA\\standard-10-5-cap385.elp'],'coord_transform',[-0.25423 0 -8.4081 0 0.0027253 0 8.5946 -10.9643 10.4963] ,'chansel',[1:EEG.nbchan] );
+    [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
+    EEG = pop_multifit(EEG,[],'threshold',100,'plotopt',{'normlen' 'on'});
+    [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, 0);
     
     %% Remove poorly fitted components (high residual variance dipoles)
     rvFails=[];
@@ -65,67 +65,70 @@ end
 totalNumberOfFails=[];
 %% Reject with ADJUST
 if false
-try
-    %Remove bad channels if present
-    if ~isempty(badChannels)
-        EEG = pop_select( EEG,'nochannel',badChannels);
+    try
+        %Remove bad channels if present
+        if ~isempty(badChannels)
+            EEG = pop_select( EEG,'nochannel',badChannels);
+        end
+        
+        %Set output file location
+        saveAdjust = strcat(folderPath,'\Output\ProcessingInfo\',fileName,'.txt');
+        [ ADJUSTarts ] = ADJUST(EEG,saveAdjust);
+        numADJUSTFails = length(ADJUSTarts);
+        appendMethods(fid, [' The ADJUST toolbox was used in order to identify artifact components. All components identified as artifacts by ADJUST were removed from the data.']);
+        
+        %If any components have been identified as bad, reject them
+        if ~isempty(ADJUSTarts)
+            EEG = pop_subcomp( EEG, ADJUSTarts, 0);
+        end
+        
+        %Interpolate missing channels back in if necessary
+        if ~isempty(badChannels)
+            EEG = pop_interp(EEG, chanStruct, 'spherical');
+        end
+    catch
+        %If something fails, just set everything to 0 and carry on.
+        ADJUSTarts = [];
+        numADJUSTFails = 0;
     end
-    
-    %Set output file location
-    saveAdjust = strcat(folderPath,'\Output\ProcessingInfo\',fileName,'.txt');
-    [ ADJUSTarts ] = ADJUST(EEG,saveAdjust);
-    numADJUSTFails = length(ADJUSTarts);
-    appendMethods(fid, [' The ADJUST toolbox was used in order to identify artifact components. All components identified as artifacts by ADJUST were removed from the data.']);
-    
-    %If any components have been identified as bad, reject them
-    if ~isempty(ADJUSTarts)
-        EEG = pop_subcomp( EEG, ADJUSTarts, 0);
-    end
-    
-    %Interpolate missing channels back in if necessary
-    if ~isempty(badChannels)
-        EEG = pop_interp(EEG, chanStruct, 'spherical');
-    end
-catch
-    %If something fails, just set everything to 0 and carry on.
-    ADJUSTarts = [];
-    numADJUSTFails = 0;
-end
 end
 ADJUSTarts = [];
-    numADJUSTFails = 0;
-    
+numADJUSTFails = 0;
+
 %% Reject with SASICA
 if true
     try
-    %Remove bad channels if present
-    if ~isempty(badChannels)
-        EEG = pop_select( EEG,'nochannel',badChannels);
-    end
-    
-    %Run sasica
-    EEG = eeg_SASICA(EEG,PARAMETERS.SASICA);
-    
-    %Get indices of failed components
-    SASICAarts = find(EEG.reject.gcompreject==1);
-    %Count number of failed components
-    numSASICAFails = length(ADJUSTarts);
-    %reject failed components
-    EEG=pop_subcomp(EEG, SASICAarts);
-    
-    appendMethods(fid, [' The SASICA toolbox was used in order to identify artifact components. All components identified as artifacts by SASICA were removed from the data.']);
-    
-    %Interpolate missing channels back in if necessary
-    if ~isempty(badChannels)
-        EEG = pop_interp(EEG, chanStruct, 'spherical');
-    end
+        %Remove bad channels if present
+        if ~isempty(badChannels)
+            EEG = pop_select( EEG,'nochannel',badChannels);
+        end
+        
+        %Run sasica
+        EEG = eeg_SASICA(EEG,PARAMETERS.SASICA);
+        
+        %Get indices of failed components
+        SASICAarts = find(EEG.reject.gcompreject==1);
+        %Count number of failed components
+        numSASICAFails = length(ADJUSTarts);
+        %reject failed components
+        EEG=pop_subcomp(EEG, SASICAarts);
+        
+        appendMethods(fid, [' The SASICA toolbox was used in order to identify artifact components. All components identified as artifacts by SASICA were removed from the data.']);
+        
+        %Interpolate missing channels back in if necessary
+        if ~isempty(badChannels)
+            EEG = pop_interp(EEG, chanStruct, 'spherical');
+        end
     catch
+        if ~isempty(badChannels)
+            EEG = pop_interp(EEG, chanStruct, 'spherical');
+        end
         SASICAarts = [];
         numSASICAFails = 0;
-end
+    end
     
-
-%Calculate the total number of rejected components
-totalNumberOfFails = totalNumberOfFails + numADJUSTFails + numSASICAFails;
+    
+    %Calculate the total number of rejected components
+    totalNumberOfFails = totalNumberOfFails + numADJUSTFails + numSASICAFails;
 end
 
